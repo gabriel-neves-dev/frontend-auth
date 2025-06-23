75012873fcf18db660e5508d6feebd8345dc6e78
Gabriel Neves <gabrielneves.dev@gmail.com>
2025-06-23 10:43:42 -0300

    finished login and register page

---
+++ src/pages/LoginPage/index.tsx
@@ -0,0 +1,11 @@
+import LoginCard from '../../components/LoginCard';
+
+const LoginPage = () => {
+    return (
+        <div>
+            <LoginCard />
+        </div>
+    );  
+}
+
+export default LoginPage;
---
+++ src/pages/RegisterPage/index.tsx
@@ -0,0 +1,12 @@
+
+import RegisterCard from '../../components/RegisterCard';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <RegisterCard />
+        </div>
+    );  
+}
+
+export default RegisterPage;
